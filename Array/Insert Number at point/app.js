d8dd9ac0b655c795b0352b9a99f86e35c4e32016
asitkr <krsinhasaurabh@gmail.com>
2025-05-01 11:16:26 +0530

    done

--- Array/Insert Number at point/app.js
+++ Array/Insert Number at point/app.js
@@ -1,16 +1,37 @@
 const arr=[1, 12, 31, 41, 5, 90, 39];
 const num = 29; // insert after 5 and 5 remain in array
 
-const insertNumber = (arr, num) => {
+// const insertNumber = (arr, num) => {
+//     let i = 0;
+//     let x = 5;
+//     let tempArr = [];
+
+//     while(i < arr.length) {
+//         tempArr.push(arr[i]);
+
+//         if(arr[i] === x) {
+//             tempArr.push(num);
+//         }
+
+//         i++;
+//     }
+
+//     return tempArr;
+// }
+
+const insertNumberDifferent = (arr, num) => {
     let i = 0;
     let x = 5;
+    let j = 0;
     let tempArr = [];
 
     while(i < arr.length) {
-        tempArr.push(arr[i]);
+        tempArr[j] = arr[i];
+        j++;
 
         if(arr[i] === x) {
-            tempArr.push(num);
+            tempArr[j] = num;
+            j++;
         }
 
         i++;
@@ -19,4 +40,4 @@ const insertNumber = (arr, num) => {
     return tempArr;
 }
 
-console.log(insertNumber(arr, num));
+console.log(insertNumberDifferent(arr, num));
