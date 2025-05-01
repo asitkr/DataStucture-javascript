9578f3300cf8aa6a8335f318ff64991c554b8bf0
asitkr <krsinhasaurabh@gmail.com>
2025-05-01 12:01:50 +0530

    remove duplicate from an array

--- Array/Remove duplicate from Array/app.js
+++ Array/Remove duplicate from Array/app.js
@@ -5,4 +5,30 @@ const removeDuplicateElement = (arr) => {
     return    [...new Set([...arr])]
 }
 
-console.log(removeDuplicateElement(arr))
+// console.log(removeDuplicateElement(arr));
+
+
+// Alternate Brute force approach
+const removeDuplicateElementAlternate = (arr) => {
+    let tempArr = [];
+    let k = 0;
+    
+    for(let i=0; i<arr.length; i++) {
+        let isDuplicate = false;
+        for(let j=0; j<k; j++) {
+            if(arr[i] === tempArr[j]) {
+                isDuplicate = true;
+                break;
+            }
+        }
+        
+        if(!isDuplicate) {
+            tempArr.push(arr[i]);
+            k++;
+        }
+    }
+
+    return tempArr;
+}
+
+console.log(removeDuplicateElementAlternate(arr));
