7f2ca78a7862e341783b0d115a70ecae326e7a7c
asitkr <krsinhasaurabh@gmail.com>
2025-05-07 12:26:14 +0530

    get minimum number or currency note

--- Array/Get Minimum Notes Indian Currency/app.js
+++ Array/Get Minimum Notes Indian Currency/app.js
@@ -1,6 +1,7 @@
 // const arr = [1, 2, 5, 10, 20, 50, 100, 200, 500];
 
 const getMinimumNumberOfNotes = (amount) => {
+    // when we get the minimum number of currency array in descending order
     const arr = [500, 200, 100, 50, 20, 10, 5, 2, 1];
     const noteCount = {};
 
